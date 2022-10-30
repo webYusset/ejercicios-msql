a352db37d14f04fc76773e44ad005e025edd8d85
webyusset <od.yussetrodriguez@gmail.com>
2022-10-30 22:30:23 +0100

    ejercicios mysql

---
+++ ejercicio 18.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 30-10-2022 a las 17:26:35
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `usuarios`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(20) DEFAULT NULL,
+  `nombre` varchar(20) DEFAULT NULL,
+  `sexo` varchar(1) DEFAULT NULL,
+  `nivel` tinyint(4) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `marca` varchar(20) DEFAULT NULL,
+  `compania` varchar(20) DEFAULT NULL,
+  `saldo` float DEFAULT NULL,
+  `activo` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Consultar: Mostrar el email de los usuarios que no usan yahoo
+--
+
+SELECT email FROM Usuarios WHERE email NOT LIKE '%yahoo.com';
+('brenda@live.com'),
+('oscar@gmail.com'),
+('francisco@gmail.com'),
+('enrique@outlook.com'),
+('luis@hotmail.com'),
+('daniel@outlook.com'),
+('jaqueline@outlook.com'),
+('roman@gmail.com'),
+('blas@hotmail.com'),
+('jessica@hotmail.com'),
+('diana@live.com'),
+('ricardo@hotmail.com'),
+('valentina@live.com'),
+('brenda2@gmail.com'),
+('lucia@gmail.com'),
+('juan@outlook.com'),
+('elpidio@outlook.com'),
+('jessica2@live.com'),
+('luis2@live.com'),
+('hugo@live.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Usuarios`
+--
+ALTER TABLE `Usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Usuarios`
+--
+ALTER TABLE `Usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
